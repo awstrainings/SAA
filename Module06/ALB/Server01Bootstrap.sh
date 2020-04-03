530ba5f454c2fc56a0af8080230c47b0362f8936
awstrainings <63106516+awstrainings@users.noreply.github.com>
2020-04-03 10:08:06 -0400

    Add files via upload

---
+++ Module06/ALB/Server01Bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd php
+cd /var/www/html
+wget https://github.com/awstrainings/SAA/raw/master/Module06/ALB/RoundCheetah.jpg
+mv RoundCheetah.jpg RoundAnimal.jpg
+wget https://github.com/awstrainings/SAA/raw/master/Module06/ALB/index.php
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
